1bf1fb0cbfcf0940a9811c9354c2d8674009cefe
vladimirstempel <stempelv@gmail.com>
2023-12-03 15:32:27 +0100

    [MODULE-3] add homework 4

---
+++ ExtensionMethods/Delegates/Delegates.cs
@@ -0,0 +1,11 @@
+﻿namespace ExtensionMethods;
+
+public delegate int Operation(int x, int y);
+
+internal static class Delegates
+{
+    public static int Sum(int x, int y)
+    {
+        return x + y;
+    }
+}
