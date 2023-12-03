6881e21a05b0468f6d214771713d3ea5c4055fd6
vladimirstempel <stempelv@gmail.com>
2023-12-03 12:44:06 +0100

    [MODULE-3] add homework 5

---
+++ HelloWorldAsync/HelloWorldAsync/FileReader.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace HelloWorldAsync;
+
+internal static class FileReader
+{
+    public static async Task<string> ReadHelloAsync(string pathToFile = "")
+    {
+        return await File.ReadAllTextAsync(pathToFile + "hello.txt");
+    }
+    
+    public static async Task<string> ReadWorldAsync(string pathToFile = "")
+    {
+        return await File.ReadAllTextAsync(pathToFile + "world.txt");
+    }
+
+    public static Task<string> CombineHelloWorldAsync(Task<string> hello, Task<string> world)
+    {
+        return Task.WhenAll(hello, world)
+            .ContinueWith(task => string.Join(" ", task.Result));
+    }
+}
---
+++ HelloWorldAsync/HelloWorldAsync/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HelloWorldAsync;
+
+public static class Program
+{
+    public static async Task Main()
+    {
+        var helloTask = FileReader.ReadHelloAsync();
+        var worldTask = FileReader.ReadWorldAsync();
+        var helloWorld = await FileReader.CombineHelloWorldAsync(helloTask, worldTask);
+        
+        Console.WriteLine(helloWorld);
+    }
+}
