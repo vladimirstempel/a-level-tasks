7f013cf95d80137f8b9f11e7e99bc22c7054fed6
vladimirstempel <stempelv@gmail.com>
2023-12-03 12:46:08 +0100

    [MODULE-3] refactor FileReader

--- HelloWorldAsync/HelloWorldAsync/FileReader.cs
+++ HelloWorldAsync/HelloWorldAsync/FileReader.cs
@@ -5,14 +5,14 @@ namespace HelloWorldAsync;
 
 internal static class FileReader
 {
-    public static async Task<string> ReadHelloAsync(string pathToFile = "")
+    public static async Task<string> ReadHelloAsync(string pathToFile = "hello.txt")
     {
-        return await File.ReadAllTextAsync(pathToFile + "hello.txt");
+        return await File.ReadAllTextAsync(pathToFile);
     }
     
-    public static async Task<string> ReadWorldAsync(string pathToFile = "")
+    public static async Task<string> ReadWorldAsync(string pathToFile = "world.txt")
     {
-        return await File.ReadAllTextAsync(pathToFile + "world.txt");
+        return await File.ReadAllTextAsync(pathToFile);
     }
 
     public static Task<string> CombineHelloWorldAsync(Task<string> hello, Task<string> world)
